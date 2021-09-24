fcb0e394896344fb9c6fdcfdc302f3c421c22ac7
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2021-09-24 13:55:57 -0500

    modified project files

--- acquire.py
+++ acquire.py
@@ -6,11 +6,21 @@ def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 #zillow db
-zillow_sql = "SELECT bathroomcnt, bedroomcnt, fips, yearbuilt, taxvaluedollarcnt, taxamount, calculatedfinishedsquarefeet, predictions_2017.logerror AS log_error\
+zillow_sql = "SELECT *\
                 FROM properties_2017\
                 LEFT JOIN predictions_2017 USING(parcelid)\
-                JOIN propertylandusetype USING(propertylandusetypeid)\
-                WHERE propertylandusetype.propertylandusetypeid LIKE '261' AND predictions_2017.transactiondate BETWEEN '2017-05-01' AND '2017-08-31';"
+                LEFT JOIN airconditioningtype USING(airconditioningtypeid)\
+                LEFT JOIN architecturalstyletype USING(architecturalstyletypeid)\
+                LEFT JOIN buildingclasstype USING(buildingclasstypeid)\
+                LEFT JOIN heatingorsystemtype USING(heatingorsystemtypeid)\
+                LEFT JOIN propertylandusetype USING(propertylandusetypeid)\
+                LEFT JOIN storytype USING(storytypeid)\
+                LEFT JOIN typeconstructiontype USING(typeconstructiontypeid)\
+                LEFT JOIN unique_properties USING(parcelid)\
+                WHERE properties_2017.id IN(\
+                SELECT DISTINCT id\
+                FROM properties_2017\
+                WHERE predictions_2017.transactiondate LIKE '2017%%') AND latitude IS NOT NULL;"
 
 #acquires zillow dataset
 def get_zillow_data():
